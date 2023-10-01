c0ee49205ec495c5890cbcdf36587610650af728
Karina Prado <karina.pradogutierrez@gmail.com>
2023-09-30 22:53:25 -0500

    Feat: refactor

--- src/pages/pet/[id].tsx
+++ src/pages/pet/[id].tsx
@@ -7,14 +7,14 @@ const PetPage = () => {
   const router = useRouter();
   const { id } = router.query;
 
-  const pet = items.find((pet: any) => pet.id === parseInt(id));
+  const pet = items.find((pet: any) => pet.id === parseInt(id as string));
   console.log(pet);
 
   return (
     <div>
       <h1>{pet?.name}</h1>
       <p>{pet?.description}</p>
-      <Image alt={pet?.name} height={200} width={200} src={pet?.url} />
+      <Image alt={pet?.name!} height={200} width={200} src={pet?.url!} />
     </div>
   );
 };
